caa154f7ddda94966ce6eb9daeaa590391977247
Michota <dawidmichota@gmail.com>
2023-09-03 20:32:01 +0200

    Root Component is now styled

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,11 +1,28 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../ui/Sidebar";
+import { styled } from "styled-components";
+
+const StyledRoot = styled.div`
+  display: grid;
+  grid-template-columns: 20rem 1fr;
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--theme-black-200);
+  color: var(--theme-white);
+`;
+
+const Main = styled.main`
+  padding: 2rem;
+`;
 
 function Root() {
   return (
-    <div>
-      <p>Hello World!</p>
-      <Outlet />
-    </div>
+    <StyledRoot>
+      {/* <Sidebar /> */}
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledRoot>
   );
 }
 
