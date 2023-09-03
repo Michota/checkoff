3f0c126d2dbd6f624de2a45eed959b51be81d741
Michota <dawidmichota@gmail.com>
2023-09-03 20:21:25 +0200

    created GlobalStyles for styling all the app

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,9 +1,40 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-p {
-  color: red;
+:root {
+  --theme-primary: #ffbd2d;
+  --theme-secondary: #ffd270;
+  --theme-white: #ececec;
+
+  --theme-black-000: #000000;
+  --theme-black-100: #111111;
+  --theme-black-200: #222222;
+  --theme-black-300: #333333;
+
+  --theme-gray-100: #4a4a4a;
+
+  
+  --theme-red: #ff7d7d;
+  --theme-green: #8aff95;
+
+  --deafult-radius: 2px;
 }
+
+*, *::after, *::before{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Lato', sans-serif;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.6rem;
+}
+
 `;
 
 export default GlobalStyles;
