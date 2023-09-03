e9222c2d1f3aa90417148aebed2e56e64527b445
Michota <dawidmichota@gmail.com>
2023-09-03 18:07:13 +0200

    created global styles file

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./routes/Root";
 import ErrorPage from "./pages/ErrorPage";
 import Checklist from "./pages/Checklist";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyles />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+p {
+  color: red;
+}
+`;
+
+export default GlobalStyles;
