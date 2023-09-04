056e6e05de0dc06dbd5720602f7f9899f28c1408
Michota <dawidmichota@gmail.com>
2023-09-04 20:53:43 +0200

    MAX_WORD_LENGTH variable added
    if the word is longer than MAX_WORD_LENGTH, the text is truncated where the length of the characters is exceeded

--- src/utils/stringShortener.js
+++ src/utils/stringShortener.js
@@ -3,6 +3,10 @@ export default function stringShortener(
   NUMBER_OF_WORDS = 5,
   ELIPSIS = "..."
 ) {
+  if (typeof string !== "string") throw new Error("This is not a string!");
+
+  const MAX_WORD_LENGTH = 32;
+
   const elipsis = ELIPSIS || "";
 
   const words =
@@ -11,5 +15,8 @@ export default function stringShortener(
       .map((word, index) => (index < NUMBER_OF_WORDS ? word : ""))
       .join(" ")
       .trim() + elipsis;
-  return words;
+
+  return words.length > MAX_WORD_LENGTH
+    ? words.slice(0, MAX_WORD_LENGTH) + elipsis
+    : words;
 }
