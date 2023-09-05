b23a7ad893f220e984b69e42bdde59e43bd703a9
Michota <dawidmichota@gmail.com>
2023-09-05 20:25:08 +0200

    Components: created reusable box component

---
+++ src/ui/Box.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+const Box = styled.div`
+  padding: 1rem;
+  font-size: 1.6rem;
+  overflow: auto;
+  display: flex;
+  align-items: center;
+  border-radius: var(--deafult-radius);
+  box-shadow: var(--drop-shadow);
+`;
+
+export default Box;
