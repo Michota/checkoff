ae557b6f09bda8308e9a42b116bbf300a3c85692
Michota <dawidmichota@gmail.com>
2023-09-09 15:32:00 +0200

    style: ButtonAdd, StyledButton

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,10 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledButton = styled.button`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 2.4rem;
   height: 2.4rem;
   border: none;
@@ -8,12 +12,19 @@ const StyledButton = styled.button`
   border-radius: var(--deafult-radius);
   font-weight: bold;
   color: white;
-  cursor: pointer;
 
   &:hover {
     transform: scale(105%);
   }
   transition: transform 100ms;
+
+  ${(props) =>
+    props.$floating === true &&
+    css`
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    `};
 `;
 
 const ButtonClose = styled(StyledButton)`
@@ -21,12 +32,35 @@ const ButtonClose = styled(StyledButton)`
   background-color: var(--theme-red);
 `;
 
-function Button({ children, onClick, type = "default" }) {
+const ButtonAdd = styled(StyledButton)`
+  border: none;
+  background-color: var(--theme-green);
+  color: var(--theme-black-200);
+  font-size: 2rem;
+  width: 4rem;
+  height: 4rem;
+`;
+
+function Button({ children, onClick, type = "default", $floating = false }) {
   switch (type) {
     case "default":
-      return <StyledButton onClick={onClick}>{children}</StyledButton>;
+      return (
+        <StyledButton $floating={$floating} onClick={onClick}>
+          {children}
+        </StyledButton>
+      );
     case "close":
-      return <ButtonClose onClick={onClick}>{children}</ButtonClose>;
+      return (
+        <ButtonClose $floating={$floating} onClick={onClick}>
+          {children}
+        </ButtonClose>
+      );
+    case "add":
+      return (
+        <ButtonAdd $floating={$floating} onClick={onClick}>
+          {children}
+        </ButtonAdd>
+      );
   }
 }
 
