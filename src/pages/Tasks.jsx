7106ec4e85ca64a0bc0122b314e6629c61d3cebb
Michota <dawidmichota@gmail.com>
2023-09-09 11:45:43 +0200

    new: early version of debouncing updates was made

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -3,6 +3,8 @@ import Task from "../components/Task";
 import useTaskData from "../components/useTaskData";
 import TaskDetails from "../components/TaskDetails";
 import { useEffect, useState } from "react";
+import useUpdateTask from "../components/useTaskUpdate";
+import { useDebounce } from "../hooks/useDebounce";
 
 const StyledTasksPanel = styled.div`
   display: grid;
@@ -19,8 +21,21 @@ const StyledChecklist = styled.div`
 function Tasks() {
   const { isLoading, tasksState, setTasksState } = useTaskData();
   const [selectedTaskId, setSelectedTaskId] = useState(null);
+  const { updateTask, isUpdating } = useUpdateTask();
+  const debounce = useDebounce(tasksState, 5000);
 
-  function handleSetTasksState(updatedTask) {
+  useEffect(
+    function () {
+      if (!selectedTaskId) return;
+      if (debounce)
+        updateTask(
+          debounce?.find((task) => task.id === selectedTaskId ?? undefined)
+        );
+    },
+    [debounce, selectedTaskId, updateTask]
+  );
+
+  async function handleSetTasksState(updatedTask) {
     const otherTasks = tasksState.filter((task) => task.id !== updatedTask.id);
     const index = tasksState.findIndex((task) => task.id === updatedTask.id);
     // * newly updated task is at the bottom...
