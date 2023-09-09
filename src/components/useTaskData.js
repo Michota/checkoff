0dfedcec8ed6f7912bd7fe02725e2afd29cc1233
Michota <dawidmichota@gmail.com>
2023-09-09 15:29:48 +0200

    docs: some early docs were created

--- src/components/useTaskData.js
+++ src/components/useTaskData.js
@@ -2,6 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getTasksData } from "../services/tasksAPI";
 import { useEffect, useState } from "react";
 
+/**
+ * Stare for storing tasks data and updating it locally.
+ * @param taskState - array containing all the tasks
+ * @param setTasksState - setter function for updating local state
+ */
 function useTaskData() {
   const {
     isLoading,
@@ -11,6 +16,8 @@ function useTaskData() {
     queryKey: ["tasks"],
     queryFn: getTasksData,
   });
+
+  // * Placing local taskState here makes it easy to manipulate data locally without changing anything in remote state
 
   const [tasksState, setTasksState] = useState(tasks);
 
