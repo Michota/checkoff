eb8c3a58b4382e998e8ed3dbdb1429e0b2dbb26d
Michota <dawidmichota@gmail.com>
2023-09-10 12:43:21 +0200

    feat: new way to specify options
    - method: "words"/"letters" - split by words or by letters
    - maxLength: maxLength of

--- src/utils/stringShortener.js
+++ src/utils/stringShortener.js
@@ -1,22 +1,33 @@
 export default function stringShortener(
   string,
-  NUMBER_OF_WORDS = 5,
-  ELIPSIS = "..."
+  OPTIONS = { method: "words", maxLength: 5, elipsis: "..." }
 ) {
   if (typeof string !== "string") throw new Error("This is not a string!");
 
-  const MAX_WORD_LENGTH = 32;
+  const elipsis = OPTIONS.elipsis || "";
+  if (OPTIONS.method === "words") {
+    const MAX_WORD_LENGTH = 16;
 
-  const elipsis = ELIPSIS || "";
+    const words =
+      string
+        .split(" ")
+        .map((word, index) => (index < OPTIONS.maxLength ? word : ""))
+        .join(" ")
+        .trim() + elipsis;
 
-  const words =
-    string
-      .split(" ")
-      .map((word, index) => (index < NUMBER_OF_WORDS ? word : ""))
-      .join(" ")
-      .trim() + elipsis;
+    return words.length > MAX_WORD_LENGTH
+      ? words.slice(0, MAX_WORD_LENGTH) + elipsis
+      : words;
+  } else {
+    const letters =
+      string
+        .split("")
+        .map((letter, index) => (index < OPTIONS.maxLength ? letter : ""))
+        .join("")
+        .trim() + elipsis;
 
-  return words.length > MAX_WORD_LENGTH
-    ? words.slice(0, MAX_WORD_LENGTH) + elipsis
-    : words;
+    return letters.length > OPTIONS.maxLength
+      ? letters.slice(0, OPTIONS.maxLength) + elipsis
+      : letters;
+  }
 }
