836fe1af1c06e08c884d52baa471ee9d93d27176
Michota <dawidmichota@gmail.com>
2023-09-23 16:27:12 +0200

    style: temporary login page styling

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,24 +2,40 @@ import { Form } from "react-router-dom";
 import styled from "styled-components";
 import Button from "../../ui/Button";
 import { useState } from "react";
-import { loginApi } from "../../services/apiAuth";
 import { useLogin } from "./useLogin";
-
-const StyledLoginForm = styled.div`
-  display: grid;
-  flex-direction: column;
-  gap: 2rem;
-`;
 
 const StyledInput = styled.input`
   background-color: var(--theme-white-100);
   color: var(--theme-black-200);
+  height: 1rem;
+  padding: 2rem 1rem;
 `;
 
 const InputContainer = styled.span`
-  display: flex;
+  display: grid;
+  grid-template-columns: 10rem 100%;
+  gap: 2rem;
   align-items: center;
-  gap: 1rem;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  justify-content: center;
+  align-items: center;
+  width: 40vw;
+`;
+
+const StyledLabel = styled.label`
+  font-size: 1.8rem;
+  font-weight: 600;
+`;
+
+const LoginButton = styled(Button)`
+  background-color: var(--theme-primary);
+  color: var(--theme-black-200);
+  padding: 1rem 2rem;
 `;
 
 function LoginForm() {
@@ -42,10 +58,10 @@ function LoginForm() {
   }
 
   return (
-    <StyledLoginForm>
-      <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
+      <FormContainer>
         <InputContainer>
-          <label htmlFor="email">E-mail</label>
+          <StyledLabel htmlFor="email">E-mail</StyledLabel>
           <StyledInput
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -55,7 +71,7 @@ function LoginForm() {
           />
         </InputContainer>
         <InputContainer>
-          <label htmlFor="pass">Password</label>
+          <StyledLabel htmlFor="pass">Password</StyledLabel>
           <StyledInput
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -64,9 +80,9 @@ function LoginForm() {
             id="pass"
           />
         </InputContainer>
-        <Button type="submit">Submit</Button>
-      </Form>
-    </StyledLoginForm>
+        <LoginButton type="submit">Log in</LoginButton>
+      </FormContainer>
+    </Form>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,21 @@
 import styled from "styled-components";
 import LoginForm from "../features/authentication/LoginForm";
+import Logo from "../ui/Logo";
 import { useUser } from "../features/authentication/useUser";
 import { useNavigate } from "react-router";
 import { useEffect } from "react";
 
 const StyledLogin = styled.div`
-  display: grid;
-  grid-template-columns: max-content 1fr;
   width: 100vw;
   height: 100vh;
   overflow: hidden;
   background-color: var(--theme-black-200);
   color: var(--theme-white-100);
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 4.8rem;
 `;
 
 function Login() {
@@ -30,6 +31,7 @@ function Login() {
 
   return (
     <StyledLogin>
+      <Logo size="40rem" />
       <LoginForm />
     </StyledLogin>
   );
