e621792e512824142502b2b82eabef0c4e4b8902
Michota <dawidmichota@gmail.com>
2023-09-25 21:07:54 +0200

    feat: update remote state with debounce
    local state is 100% synchronous, but remote is updated after a while

--- src/features/tasks/useManageTaskData.js
+++ src/features/tasks/useManageTaskData.js
@@ -10,21 +10,23 @@ export function useManageTaskData() {
     tasksState: tasks,
     setTasksState: setTasks,
     isLoading: isLoadingTasks,
+    setLocalAndUpdate,
   } = useTaskData();
   const { updateTask: update, isUpdating: isUpdatingTasks } = useUpdateTask();
 
   function saveAndUpdateTask(updatedTask) {
-    setTasks((tasks) => {
-      return tasks.map((task) =>
-        task.id !== updatedTask.id ? task : updatedTask
-      );
-    });
+    // const updatedLocalTasks = tasks.map((task) =>
+    //   task.id !== updatedTask.id ? task : updatedTask
+    // );
+    setLocalAndUpdate(
+      tasks.map((task) => (task.id !== updatedTask.id ? task : updatedTask))
+    );
 
-    const isCurrentlyUpdating = currentlyUpdatedTask?.id !== updatedTask?.id;
-    debounce(() => {
-      if (currentlyUpdatedTask?.id) update(currentlyUpdatedTask);
-    }, isCurrentlyUpdating);
-    currentlyUpdatedTask = updatedTask;
+    // const isCurrentlyUpdating = currentlyUpdatedTask?.id !== updatedTask?.id;
+    // debounce(() => {
+    //   if (currentlyUpdatedTask?.id) update(currentlyUpdatedTask);
+    // }, isCurrentlyUpdating);
+    // currentlyUpdatedTask = updatedTask;
   }
 
   return {
--- src/features/tasks/useTaskData.js
+++ src/features/tasks/useTaskData.js
@@ -1,14 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import { getTasksData } from "../../services/tasksAPI";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import toast from "react-hot-toast";
 import { useUser } from "../authentication/useUser";
+import createDebounce from "../../utils/debounce";
+import useUpdateTask from "./useTaskUpdate";
+import { useDebouncedCallback } from "use-debounce";
+
+// const { debounce } = createDebounce();
 
 /**
  * Stare for storing tasks data and updating it locally.
  * @param taskState - array containing all the tasks
- * @param setTasksState - setter function for updating local state
+ * @param setLocalTaskState - setter function for updating local state
  */
+
 function useTaskData() {
   const { user } = useUser();
   const {
@@ -43,6 +49,42 @@ function useTaskData() {
 
   const [tasksState, setTasksState] = useState(tasks);
 
+  // const { processChange: debounce } = createDebounce();
+  const { updateTask: update, isUpdating: isUpdatingTasks } = useUpdateTask();
+
+  // const isInitialMount = useRef(true);
+
+  // useEffect(() => {
+  //   if (isInitialMount.current) {
+  //     isInitialMount.current = false;
+  //   } else {
+  //     // Your useEffect code here to be run on update
+  //     console.log("UPDATE!!!");
+  //   }
+  // });
+  const debounced = useDebouncedCallback(() => {
+    const updatedTasks = tasksState.filter(
+      (task) =>
+        JSON.stringify(task) !==
+        JSON.stringify(tasks.find((oldTask) => oldTask.id === task.id))
+    );
+    updatedTasks.forEach((task) => update(task));
+  }, 3000);
+
+  function setLocalAndUpdate(newLocalTasksState) {
+    // 1. update tasks locally
+    // 2. debounce
+    // 3. if debounced timeout, run update for every
+    // 3.1 tasksState.forEach(task => JSON.stringify(task) === JSON.stringify(oldTasks.find(oldTask.id === task.id)))
+    setTasksState(newLocalTasksState);
+    // console.log(newLocalTasksState);
+
+    // console.log(updatedTasks);
+    debounced();
+    // updatedTasks.forEach((task) => update(task));
+    // console.log(debouncedMemo);
+  }
+
   useEffect(
     function () {
       setTasksState(tasks);
@@ -50,7 +92,7 @@ function useTaskData() {
     [tasks]
   );
 
-  return { isLoading, tasksState, setTasksState, error };
+  return { isLoading, tasksState, error, setLocalAndUpdate };
 }
 
 export default useTaskData;
