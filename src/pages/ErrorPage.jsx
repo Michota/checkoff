ab17b1ba263a9d64f9e5ed806fe1bbd30f91acf3
Michota <dawidmichota@gmail.com>
2023-09-26 21:58:56 +0200

    feat: styled error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,29 @@
 import { useRouteError } from "react-router";
+import styled from "styled-components";
+
+const StyledErrorPage = styled.div`
+  background-color: var(--theme-black-100);
+  color: var(--theme-red);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
 
 function PageNotFound() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div>
-      <h1>Error has occured!</h1>
-      <h2>{error.status}</h2>
-      <p>{error.statusText || error.message}</p>
-    </div>
+    <StyledErrorPage>
+      <span>
+        <h2>Error has occured!</h2>
+        <h1>{error.status}</h1>
+        <p>{error.statusText || error.message}</p>
+      </span>
+    </StyledErrorPage>
   );
 }
 
