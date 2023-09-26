a9073f56138af7806d54c37c8f261a885f803dd8
Michota <dawidmichota@gmail.com>
2023-09-26 17:15:18 +0200

    style: adapted to the app theme

--- src/components/EditorComponent.jsx
+++ src/components/EditorComponent.jsx
@@ -1,31 +1,43 @@
-import styled from "styled-components";
-
-import "../styles/editor.css";
 import { Editor } from "novel";
-import { useState } from "react";
+import styled from "styled-components";
 import { useTaskContext } from "./Task";
 
-const StyledEditorDiv = styled.div`
-  /* color: initial; */
+import "../styles/noveltippy.css";
+
+const StyledEditorDiv = styled(Editor)`
   width: 100%;
   min-width: 50rem;
   height: 100%;
+  position: relative;
+  background-color: var(--theme-black-200);
+  border: none;
 
-  /* background-color: darkgreen; */
+  .prose-lg {
+    font-size: 2rem;
+  }
+  .drag-handle {
+    background-color: var(--theme-black-400);
+  }
+
+  data-tippy-root {
+    background-color: red !important;
+  }
 `;
 
 function EditorComponent() {
   const { descjson, updateState, renderType } = useTaskContext();
   return (
-    <Editor
+    <StyledEditorDiv
       disableStorage={true}
       defaultValue={JSON.parse(descjson)}
-      onUpdate={(e) =>
-        updateState("descjson", JSON.stringify(e.getJSON().content))
-      }
+      onUpdate={(e) => {
+        // Save content
+        console.log(e);
+        updateState("descjson", JSON.stringify(e.getJSON().content));
+      }}
     >
       s
-    </Editor>
+    </StyledEditorDiv>
   );
 }
 
