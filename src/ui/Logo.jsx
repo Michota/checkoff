dbcf5796d209b036f50df7759ff97a26c2efbe38
Michota <dawidmichota@gmail.com>
2023-09-28 23:58:22 +0200

    feat: add Logo state

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,14 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { styled } from "styled-components";
 
-function Logo({ size = "250rem" }) {
+function Logo({ size = "250rem", full = true }) {
+  const source = full
+    ? `/src/assets/images/logo.png`
+    : `/src/assets/images/check.png`;
   return (
     <NavLink to="/">
-      <img
-        width={size}
-        alt="CheckOff Logo"
-        src="\src\assets\images\logo.png"
-      ></img>
+      <img width={size} alt="CheckOff Logo" src={source}></img>
     </NavLink>
   );
 }
