612477f5b932124e900cbfd52fdd26a181af61ab
Michota <dawidmichota@gmail.com>
2023-09-30 14:36:27 +0200

    feat: create separate file for minor customization

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -13,6 +13,7 @@ import TaskDetails from "../components/TaskDetails";
 import DraggableWindow from "../components/DraggableWindow";
 
 import "../styles/FullCalendar.css";
+import "../styles/FullCalendarCustom.css";
 
 // Styling Components
 
