33498217a1c55879a0f05c2418e58d8e91b959f7
Michota <dawidmichota@gmail.com>
2023-10-08 14:40:42 +0200

    style: Tooltip

--- src/components/TaskChilds/DeleteButton.jsx
+++ src/components/TaskChilds/DeleteButton.jsx
@@ -2,27 +2,30 @@ import Button from "../../ui/Button";
 import { MdDelete, MdDeleteForever } from "react-icons/md";
 import { useTaskContext } from "../Task";
 import { useGeneralTasksProvider } from "../../contexts/GeneralTasksContext";
+import { Tooltip } from "../Tooltip";
 
 function DeleteButton() {
   const { inTrash, deleteTask, id, updateState } = useTaskContext();
   const { setSelectedTaskId } = useGeneralTasksProvider();
 
   return (
-    <Button
-      btnType="delete"
-      onClick={(e) => {
-        setSelectedTaskId(null);
-        if (inTrash) {
-          e.stopPropagation();
-          deleteTask(id);
-        } else {
-          e.stopPropagation();
-          updateState("inTrash", true);
-        }
-      }}
-    >
-      {inTrash ? <MdDeleteForever /> : <MdDelete />}
-    </Button>
+    <Tooltip content="Move task to trash">
+      <Button
+        btnType="delete"
+        onClick={(e) => {
+          setSelectedTaskId(null);
+          if (inTrash) {
+            e.stopPropagation();
+            deleteTask(id);
+          } else {
+            e.stopPropagation();
+            updateState("inTrash", true);
+          }
+        }}
+      >
+        {inTrash ? <MdDeleteForever /> : <MdDelete />}
+      </Button>
+    </Tooltip>
   );
 }
 
--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,9 +1,105 @@
 import React from "react";
 import Tippy from "@tippyjs/react";
-import "../styles/Tippy.css"; // optional
+// import "../styles/Tippy.css"; // optional
+import styled from "styled-components";
 
-export function Tooltip({ children, content }) {
+const StyledTippy = styled(Tippy)`
+  & {
+    /* Arrow */
+    .tippy-arrow {
+      width: 16px;
+      height: 16px;
+      color: var(--theme-black-300);
+      .light-mode & {
+        color: var(--theme-black-000);
+      }
+    }
+    .tippy-arrow:before {
+      content: "";
+      position: absolute;
+      border-color: transparent;
+      border-style: solid;
+    }
+    .tippy-content {
+      position: relative;
+      padding: 5px 9px;
+      z-index: 1;
+    }
+  }
+
+  /* Box */
+
+  &[data-animation="fade"][data-state="hidden"] {
+    opacity: 0;
+  }
+  [data-tippy-root] {
+    max-width: calc(100vw - 10px);
+  }
+  & {
+    position: relative;
+    color: var(--theme-white-200);
+    background-color: var(--theme-black-300);
+    box-shadow: var(--drop-shadow);
+    border-radius: 4px;
+    font-size: 14px;
+    line-height: 1.4;
+    white-space: normal;
+    outline: 0;
+    transition-property: transform, visibility, opacity;
+
+    .light-mode & {
+      background-color: var(--theme-black-000);
+      color: var(--theme-white-100);
+    }
+  }
+
+  &[data-placement^="top"] > .tippy-arrow {
+    bottom: 0;
+  }
+  &[data-placement^="top"] > .tippy-arrow:before {
+    bottom: -7px;
+    left: 0;
+    border-width: 8px 8px 0;
+    border-top-color: initial;
+    transform-origin: center top;
+  }
+  &[data-placement^="bottom"] > .tippy-arrow {
+    top: 0;
+  }
+  &[data-placement^="bottom"] > .tippy-arrow:before {
+    top: -7px;
+    left: 0;
+    border-width: 0 8px 8px;
+    border-bottom-color: initial;
+    transform-origin: center bottom;
+  }
+  &[data-placement^="left"] > .tippy-arrow {
+    right: 0;
+  }
+  &[data-placement^="left"] > .tippy-arrow:before {
+    border-width: 8px 0 8px 8px;
+    border-left-color: initial;
+    right: -7px;
+    transform-origin: center left;
+  }
+  &[data-placement^="right"] > .tippy-arrow {
+    left: 0;
+  }
+  &[data-placement^="right"] > .tippy-arrow:before {
+    left: -7px;
+    border-width: 8px 8px 8px 0;
+    border-right-color: initial;
+    transform-origin: center right;
+  }
+  &[data-inertia][data-state="visible"] {
+    transition-timing-function: cubic-bezier(0.54, 1.5, 0.38, 1.11);
+  }
+`;
+
+export function Tooltip({ className, children, content }) {
   return (
-    <Tippy content={<span>{content}</span>}>{<span>{children}</span>}</Tippy>
+    <StyledTippy className={className} content={<span>{content}</span>}>
+      {<span>{children}</span>}
+    </StyledTippy>
   );
 }
