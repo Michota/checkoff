27968ec74fb35fd20f5d15fc43403d3b12fff79e
Michota <dawidmichota@gmail.com>
2023-11-25 11:44:36 +0100

    docs: add JSdocs

--- src/utils/addAlphaToHex.js
+++ src/utils/addAlphaToHex.js
@@ -1,3 +1,24 @@
+/**
+ * Adds opacity value to a hexadecimal color code.
+ *
+ * @param {string} hex - The hexadecimal color code starting with the '#' character.
+ * @param {number} opacityValue - The opacity value ranging from 0 to 100.
+ * @returns {string} - The hexadecimal color code with the added opacity value.
+ * @throws {Error} - Throws an error if the color code does not contain the '#' character.
+ *
+ * @example
+ * const hexWithOpacity = addAlphaToHex("#336699", 50);
+ * // Returns: "#33669950"
+ *
+ * @example
+ * const hexWithMaxOpacity = addAlphaToHex("#FF0000", 100);
+ * // Returns: "#FF0000"
+ *
+ * @example
+ * const hexWithoutHashtag = addAlphaToHex("336699", 50);
+ * // Throws an error: "Hex has to have hashtag!"
+ */
+
 export default function addAlphaToHex(hex, opacityValue) {
   if (!hex.includes("#")) throw new Error("Hex has to have hashtag!");
 
