ac7f4e84adf0aa7e5196c471b559dbf7ba2d46fe
Michota <dawidmichota@gmail.com>
2023-11-25 11:55:07 +0100

    feat: add onClick to IconPicker

--- src/components/IconPicker.jsx
+++ src/components/IconPicker.jsx
@@ -1,13 +1,12 @@
 import styled from "styled-components";
 import { useState } from "react";
 import DynamicMaterialIcon from "./DynamicMaterialIcon";
-import icons from "../features/iconpicker/icons";
+import icons from "../features/iconpicker/materialicons";
 
 const StyledIconPicker = styled.div.attrs({ className: "icon-picker" })`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   width: fit-content;
-  background-color: red;
   border-radius: var(--default-radius);
   padding: 0.4rem;
 `;
@@ -16,14 +15,15 @@ const IconContainer = styled.div.attrs({ className: "icon-option-container" })`
   cursor: pointer;
 `;
 
-function IconPicker({ getIconName }) {
+function IconPicker({ getIconName, onClick }) {
   const [selectedIcon, setSelectedIcon] = useState();
 
   function handleIconSelect(iconContainer) {
     const iconName = iconContainer.dataset.icon;
     if (!iconName) return;
     setSelectedIcon(iconName);
-    if (getIconName) getIconName(iconName);
+    getIconName(iconName);
+    if (onClick) onClick(onClick);
   }
 
   return (
