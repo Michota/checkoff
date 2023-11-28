338a2d540e65cdbd1fbbd21f810446eb885cf4cf
Michota <dawidmichota@gmail.com>
2023-11-28 20:47:49 +0100

    fix: make component use derived data

--- src/components/HabitGrid.jsx
+++ src/components/HabitGrid.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import HabitDay from "./HabitDay";
+import { getDateNDaysAgo } from "../utils/getDateNDaysAgo";
 
 const StyledHabitGrid = styled.div`
   /* Its default background-color. The actuall color is set in Habit component */
@@ -9,13 +10,36 @@ const StyledHabitGrid = styled.div`
   grid-template-rows: repeat(7, 1rem);
   gap: 2px;
   width: fit-content;
-`;
+function stretchToYearData(arr, targetSize) {
+  const currentSize = arr.length;
+
+  if (currentSize >= targetSize) {
+    return arr; // Array is already of the desired size
+  }
+  const remainingSize = targetSize - currentSize;
+  const defaultObject = {
+    score: 0,
+  }; // Change this to the default object you want to add
+
+  for (let i = 0; i < remainingSize; i++) {
+    arr.push(defaultObject);
+  }
+
+  return arr;
+}
 
 function HabitGrid({ data }) {
+  const gridSize = 364; // Make grid perfectly fit in rectangle.
+  const lastYear = stretchToYearData(data, gridSize);
   return (
     <StyledHabitGrid>
-      {data.map((level, i) => (
-        <HabitDay key={i} data={data[i]} />
+      {lastYear.map((level, i) => (
+        <HabitDay
+          key={i}
+          data={
+            data[i].score ? data[i] : { ...data[i], date: getDateNDaysAgo(i) }
+          }
+        />
       ))}
     </StyledHabitGrid>
   );
